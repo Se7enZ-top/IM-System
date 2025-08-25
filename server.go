99704920cb7ee240b360f1603a124678273b3313
Se7en <qi230312@gmail.com>
2025-08-25 21:05:08 +0800

    v1.1.0

--- server.go
+++ server.go
@@ -2,89 +2,104 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
 
 type Server struct {
-	Ip        string
-	Port      int
-	OnlineMap map[string]*User
-	maplock   sync.RWMutex
+	Ip   string
+	Port int
 
-	Message chan string
+	OnlineMap map[string]*User
+	Maplock   sync.RWMutex
+
+	message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:        ip,
-		Port:      port,
+		Ip:   ip,
+		Port: port,
+
 		OnlineMap: make(map[string]*User),
-		Message:   make(chan string),
+		message:   make(chan string),
 	}
-
 	return server
-
 }
 
-// 监听message广播消息的办法 一旦有消息发送给全部在线用户
-func (this *Server) ListenMessager() {
-	for {
-		msg := <-this.Message
-		//将发送
-		this.maplock.Lock()
-		for _, cli := range this.OnlineMap {
-			cli.C <- msg
-		}
-		this.maplock.Unlock()
-	}
-}
-
-// 广播消息的办法
-func (this *Server) BroadCast(user *User, msg string) {
-	sendMsg := "[" + user.Addr + "]" + user.Name + msg
-
-	this.Message <- sendMsg
+func (this *Server) BoardCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.message <- sendMsg
 }
 
 func (this *Server) Handler(conn net.Conn) {
-	//具体业务逻辑
-	user := NewUser(conn)
+	fmt.Println("Connection Successed!" + conn.RemoteAddr().String())
 
-	this.maplock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.maplock.Unlock()
+	user := NewUser(conn, this)
 
-	this.BroadCast(user, "已上线")
+	user.Online()
+
+	go func() {
+		buf := make([]byte, 4096)
+
+		for {
+			n, err := conn.Read(buf)
+
+			if n == 0 {
+				user.Offline()
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("err:", err)
+				return
+
+			}
+			msg := string(buf[:n-1])
+			fmt.Println("[" + user.Addr + "]" + user.Name + ":" + msg)
+			user.DoMessage(msg)
+
+		}
+
+	}()
 
 	select {}
+}
+
+func (this *Server) ListenMessager() {
+	for {
+		sendMsg := <-this.message
+
+		this.Maplock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- sendMsg
+		}
+		this.Maplock.Unlock()
+
+	}
 
 }
 
 func (this *Server) Start() {
-	//listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
-		fmt.Println("Net err:", err)
+		fmt.Println("err:", err)
 		return
 	}
-	//close
+
 	defer listener.Close()
 
-	//启动监听上线消息
 	go this.ListenMessager()
 
 	for {
-		//accept
-		con, err := listener.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Accept err", err)
+			fmt.Println("err", err)
 			continue
 		}
 
-		//do habdler
-		go this.Handler(con)
-
+		go this.Handler(conn)
 	}
 
 }
--- user.go
+++ user.go
@@ -3,25 +3,26 @@ package main
 import "net"
 
 type User struct {
-	Name string
-	Addr string
-	C    chan string
-	conn net.Conn
+	Name   string
+	Addr   string
+	C      chan string
+	conn   net.Conn
+	server *Server
 }
 
-func NewUser(conn net.Conn) *User {
-	userAddr := conn.RemoteAddr().String()
-
+func NewUser(conn net.Conn, server *Server) *User {
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		conn: conn,
+		Name:   conn.RemoteAddr().String(),
+		Addr:   conn.RemoteAddr().String(),
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
 	}
 
 	go user.ListenMessage()
 
 	return user
+
 }
 
 func (this *User) ListenMessage() {
@@ -30,4 +31,30 @@ func (this *User) ListenMessage() {
 
 		this.conn.Write([]byte(msg + "\n"))
 	}
+}
+
+func (this *User) Online() {
+	this.server.Maplock.Lock()
+
+	this.server.OnlineMap[this.Name] = this
+
+	this.server.Maplock.Unlock()
+
+	this.server.BoardCast(this, "已上线")
+
+}
+
+func (this *User) Offline() {
+	this.server.Maplock.Lock()
+
+	delete(this.server.OnlineMap, this.Name)
+
+	this.server.Maplock.Unlock()
+
+	this.server.BoardCast(this, "下线")
+
+}
+
+func (this *User) DoMessage(msg string) {
+	this.server.BoardCast(this, msg)
 }
